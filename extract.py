5025e897bcd53338bb78101e33366e7fcb40550d
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2020-02-05 02:16:54 +0100

    Fetch a PR of litex-buildenv until it's merged to master
    This bumps Zephyr SDK.

--- extract.py
+++ extract.py
@@ -3,10 +3,14 @@ import sys
 snippets = tuttest.get_snippets(sys.argv[1])
 
 code = []
-setup = snippets['setup']['text']
+setup_lines = snippets['setup']['text'].split('\n')
 import os
 if os.path.exists('litex-buildenv'):
-    setup = "\n".join(setup.split('\n')[1:])
+    setup = "\n".join(setup_lines[1:])
+else:
+    setup_lines[1] = setup_lines[1] + " && git fetch origin pull/322/head:322-pr && git checkout 322-pr"
+    setup = "\n".join(setup_lines)
+
 
 code.append(setup)
 code.append("export HDMI2USB_UDEV_IGNORE=1 SKIP_IMAGE=1")
